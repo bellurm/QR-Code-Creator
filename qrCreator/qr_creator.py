d7d0b3202417be222e8da84c1f41b0f47da23cc4
Cyber Worm <72599968+bellurm@users.noreply.github.com>
2022-11-27 01:10:50 +0300

    Update qr_creator.py

--- qrCreator/qr_creator.py
+++ qrCreator/qr_creator.py
@@ -1,3 +1,40 @@
+import qrcode, webbrowser, sys
+
+sys.stdout.write("Press q and Enter for quit.\n")
+choice = ["y", "n", "q"]
+while True:
+	try:
+		qr_addr = input("Address: ")
+		my_qr = qrcode.make(qr_addr)
+
+		if(qr_addr == choice[-1]):
+			sys.exit()
+
+		qr_save = input("Choose a name: ")
+		my_qr.save(qr_save)
+
+		my_qr.show()
+
+		#For Linux users
+		if sys.platform.startswith('linux'):
+			choose = input("Do you want to check your address of QR code? (y / n) > ")
+			if(choose == choice[0]):
+				webbrowser.open(qr_addr)
+				sys.exit()
+			elif(choose == choice[1]):
+				sys.exit()
+			else:
+				print("Error: Invalid input.\n")
+		#For Linux users
+
+	except SyntaxError:
+		sys.stderr.write("Error: If you are a Linux user, you should use the python3.\n")
+		sys.stderr.flush()
+		sys.exit()
+	except KeyboardInterrupt:
+		sys.exit()
+"""
+##### OLD CODE - SEP 8 2021 ####
 import qrcode
 import webbrowser
 
@@ -33,3 +70,4 @@ while True:
 		break
 	except KeyboardInterrupt:
 		break
+"""
